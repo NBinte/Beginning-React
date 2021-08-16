ca6249cf7f98a52e8e9aa3343c2218271135ed01
NBinte <nuzhatbintei@gmail.com>
2021-08-16 15:03:29 +0600

    Add a counter to App component

---
+++ src/Another.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Another = (param) => {
+    return (
+        <div>
+            Another component for { param.name }
+        </div>
+    );
+}
+
+export default Another;
